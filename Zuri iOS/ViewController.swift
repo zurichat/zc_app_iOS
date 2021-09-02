bbb751f2c88899c6620f15c74d8a535d6f2c4d40
neatwhiskey <neatwhiskey24@gmail.com>
2021-09-02 12:34:15 +0100

    initial commit

---
+++ Zuri iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Zuri iOS
+//
+//  Created by Jamaaldeen Opasina on 8/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        sleep(1)
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
