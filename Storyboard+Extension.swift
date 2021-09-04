ce01b1b412b934f5f86c9a9f753c402432c1fc72
Neatwhiskey <44437144+Neatwhiskey@users.noreply.github.com>
2021-09-04 16:58:29 +0100

    Add files via upload
    created the Onboarding Screens, Closes #1

---
+++ Storyboard+Extension.swift
@@ -0,0 +1,13 @@
+//
+//  Storyboard+Extension.swift
+//  Zuri iOS
+//
+//  Created by Jamaaldeen Opasina on 9/4/21.
+//
+
+import UIKit
+
+extension UIStoryboard {
+    static let onboarding = UIStoryboard(name: "Onboarding", bundle: nil)
+    static let main = UIStoryboard(name: "Main", bundle: nil)
+}
---
+++ ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Zuri iOS
+//
+//  Created by Jamaaldeen Opasina on 8/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        sleep(3)
+        // Do any additional setup after loading the view.
+    }
+    
+    
+//    override func viewDidAppear(_ animated: Bool) {
+//        if LandscapeManager.shared.isFirstLaunch {
+//            performSegue(withIdentifier: "toOnboarding", sender: nil)
+//            LandscapeManager.shared.isFirstLaunch = true
+//        } else {
+//            performSegue(withIdentifier: "toMain", sender: nil)
+//        }
+//    }
+
+
+}
+
